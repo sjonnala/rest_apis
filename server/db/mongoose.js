593a27d03b872e067a1f888b6a9cd712992040c2
Satish <satjonna@cisco.com>
2018-01-29 11:41:47 -0800

    Test the delete /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ let db = {
     localhost: process.env.MONGODB_URI,
     mlab: 'mongodb://root:Test123@ds117888.mlab.com:17888/node-rest-apis'
 };
-console.log('MONGODB_URI: ', process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
-console.log('port: ', port);
 
 app.use(bodyParser.json());
 
@@ -21,7 +20,7 @@ app.post('/todos', (req, res) => {
     
     todo.save().then((doc) => {
         res.send(doc);
-    }, (e) => {
+    }).catch((e) => {
         res.status(400).send(e);
     })
 });
@@ -29,7 +28,7 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
-    }, (e) => {
+    }).catch((e) => {
         res.status(400).send(e);
     })
 });
@@ -43,14 +42,32 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById({_id: id}).then((todo) => {
         if(!todo){
-            return res.send(404);
+            return res.status(404).send();
         }
         res.send({todo});
-    }, (e) => {
+    }).catch((e) => {
         res.send(400);
     });
     
 })
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(404).send();
+    })
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
