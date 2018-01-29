2994c79719227e6e7b941a6294b04ff7ece88b46
Satish <satjonna@cisco.com>
2018-01-29 12:27:53 -0800

    Add Tests for patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -91,7 +91,7 @@ app.patch('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
 
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
         res.status(404).send();
     })
