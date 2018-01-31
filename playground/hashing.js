234f2c2a3fb43f24c78301c8cd0cf90ebc6a258b
Satish <satjonna@cisco.com>
2018-01-30 20:31:07 -0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = '123abc';
 
-var token = jwt.sign(data, '123');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decode = jwt.verify(token, '123');
-console.log(decode);
+var hashedPassword = '$2a$10$mfyfsLbuWZyBj/ow2ATSQ.z8Fc7Flv1zKBjMJypmjd76my7DkiGru';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data, '123');
+// console.log(token);
+
+// var decode = jwt.verify(token, '123');
+// console.log(decode);
 
 /* var message = 'I am user 3';
 var hash = SHA256(message).toString();
